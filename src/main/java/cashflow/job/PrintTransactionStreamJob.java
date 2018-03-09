cba8265a47ff7697cb8d120759de721a492442ee
Matheus Cunha <mathcunha@gmail.com>
2018-03-09 00:23:01 +0000

    migrating JMS receiver to project mathcunha/jms-spark-reciever

--- src/main/java/cashflow/job/PrintTransactionStreamJob.java
+++ src/main/java/cashflow/job/PrintTransactionStreamJob.java
@@ -1,5 +1,8 @@
 package cashflow.job;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.streaming.Durations;
@@ -7,16 +10,27 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import cashflow.Transaction;
-import cashflow.queue.JMSReciever;
+import receiver.jms.JMSReciever;
 
 public class PrintTransactionStreamJob {
 	public static void main(String[] args) throws InterruptedException {
 		SparkConf sparkConf = new SparkConf().setAppName("PrintTransactionStreamJob");
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(30));		
-		JavaReceiverInputDStream<String> transactions = jssc.receiverStream(new JMSReciever("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "TEST-QUEUE", StorageLevels.MEMORY_AND_DISK_SER_2));
+		//JavaReceiverInputDStream<String> transactions = jssc.receiverStream(new JMSReciever("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "TEST-QUEUE", StorageLevels.MEMORY_AND_DISK_SER_2));
+		JavaReceiverInputDStream<String> transactions = jssc.receiverStream(JMSReciever.buildJMSActiveMQReciever(loadProperties("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "TEST-QUEUE"), StorageLevels.MEMORY_AND_DISK_SER_2));
 		transactions.map(Transaction::fromJson).print();
 		
 		jssc.start();              // Start the computation
 		jssc.awaitTermination();   // Wait for the computation to terminate
 	}
+	
+	public static Map<String, String> loadProperties(String brokerURL, String queue){
+		Map<String, String> map = new HashMap<>();
+		map.put("brokerURL", brokerURL);
+		map.put("queue", queue);
+		map.put("user", "admin");
+		map.put("password", "admin");		
+		return map;
+	}
+	
 }
--- src/main/java/cashflow/job/TransactionDailyStreamJob.java
+++ src/main/java/cashflow/job/TransactionDailyStreamJob.java
@@ -1,5 +1,8 @@
 package cashflow.job;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.streaming.Durations;
@@ -9,14 +12,15 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import cashflow.Transaction;
-import cashflow.queue.JMSReciever;
+import receiver.jms.JMSReciever;
 import scala.Tuple2;
 
 public class TransactionDailyStreamJob {
 	public static void main(String[] args) throws InterruptedException {
 		SparkConf sparkConf = new SparkConf().setAppName("TransactionDailyStreamJob");
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(30));		
-		JavaReceiverInputDStream<String> transactionsBody = jssc.receiverStream(new JMSReciever("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "ACCOUNT-QUEUE", StorageLevels.MEMORY_AND_DISK_SER_2));
+		//JavaReceiverInputDStream<String> transactionsBody = jssc.receiverStream(new JMSReciever("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "ACCOUNT-QUEUE", StorageLevels.MEMORY_AND_DISK_SER_2));
+		JavaReceiverInputDStream<String> transactionsBody = jssc.receiverStream(JMSReciever.buildJMSActiveMQReciever(loadProperties("tcp://" + System.getenv("ACTIVEMQ_IP") + ":61616", "TEST-QUEUE"), StorageLevels.MEMORY_AND_DISK_SER_2));
 		
 		JavaDStream<Transaction> transactions = transactionsBody.map(Transaction::fromJson).repartition(2);
 		transactions = transactions.map(t -> {
@@ -30,5 +34,13 @@ public class TransactionDailyStreamJob {
 		
 		jssc.start();              // Start the computation
 		jssc.awaitTermination();   // Wait for the computation to terminate
+	}
+	public static Map<String, String> loadProperties(String brokerURL, String queue){
+		Map<String, String> map = new HashMap<>();
+		map.put("brokerURL", brokerURL);
+		map.put("queue", queue);
+		map.put("user", "admin");
+		map.put("password", "admin");		
+		return map;
 	}
 }
